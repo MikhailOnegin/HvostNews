55d60f29c17833aeaa1849e10cb44362881c741d
phoenix.worker <phoenix.worker@gmail.com>
2020-10-07 19:12:47 +0500

    Верстка LoginFragment, шрифты
    #84547#

--- app/src/main/java/ru/hvost/news/presentation/activities/MainActivity.kt
+++ app/src/main/java/ru/hvost/news/presentation/activities/MainActivity.kt
@@ -2,11 +2,22 @@ package ru.hvost.news.presentation.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+import androidx.core.content.ContextCompat
 import ru.hvost.news.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupStatusBar()
     }
+
+    private fun setupStatusBar() {
+        //window.statusBarColor = ContextCompat.getColor(this, android.R.color.white)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+    }
+
 }
