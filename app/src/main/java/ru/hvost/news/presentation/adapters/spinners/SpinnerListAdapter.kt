86798a8ae66646b85fc62db741e4fd46f9c609db
phoenix.worker <phoenix.worker@gmail.com>
2020-10-22 12:01:19 +0500

    Модернизация SpinnerListAdapter для работы с любыми списками
    #84547#

--- app/src/main/java/ru/hvost/news/presentation/adapters/spinners/SpinnerListAdapter.kt
+++ app/src/main/java/ru/hvost/news/presentation/adapters/spinners/SpinnerListAdapter.kt
@@ -8,19 +8,37 @@ import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import ru.hvost.news.R
-import ru.hvost.news.models.Species
 import ru.hvost.news.presentation.customviews.ValueHolderView
+import kotlin.reflect.KProperty1
 
-class SpeciesSpinnerAdapter(
+class SpinnerAdapter<T> private constructor(
     context: Context,
     private val resource: Int,
-    private val spinnerHint: String
-) : ArrayAdapter<Species>(context, resource) {
+    private val spinnerHint: String,
+    private val property: KProperty1<T, String>
+) : ArrayAdapter<T>(context, resource) {
+
+    /**
+     * @param context текущий контекст
+     * @param spinnerHint подсказка для спиннера
+     * @param collection список объектов для отображения в выпадающем списке
+     * @param property свойство типа String элемента коллекции для отображения
+     */
+    constructor(
+        context: Context,
+        spinnerHint: String,
+        collection: Collection<T>,
+        property: KProperty1<T, String>
+    ) : this(context, R.layout.spinner_dropdown_view, spinnerHint, property) {
+        addAll(collection)
+    }
 
     override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
         val view = convertView ?: LayoutInflater.from(context).inflate(resource, parent, false)
         view.findViewById<TextView>(R.id.spinnerText)?.apply {
-            text = getItem(position)?.speciesName
+            getItem(position)?.run {
+                text = property.get(this)
+            }
         }
         return view
     }
@@ -31,8 +49,11 @@ class SpeciesSpinnerAdapter(
             ConstraintLayout.LayoutParams.MATCH_PARENT,
             context.resources.getDimension(R.dimen.widgetsHeight).toInt()
         )
-        valueHolder.setSelection(getItem(position)?.speciesName)
+        getItem(position)?.run {
+            valueHolder.setSelection(property.get(this))
+        }
         valueHolder.setHint(spinnerHint)
         return valueHolder
     }
+
 }
