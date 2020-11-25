3a890c17dba962ac15a67f653c6735887415bb54
phoenix.worker <phoenix.worker@gmail.com>
2020-11-25 19:09:12 +0500

    Начаты работы по экрану товаров интернет-магазина
    #87365#

--- app/src/main/java/ru/hvost/news/presentation/fragments/shop/ShopFragment.kt
+++ app/src/main/java/ru/hvost/news/presentation/fragments/shop/ShopFragment.kt
@@ -1,5 +1,46 @@
 package ru.hvost.news.presentation.fragments.shop
 
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import ru.hvost.news.databinding.FragmentShopBinding
 
-class ShopFragment : Fragment()
+class ShopFragment : Fragment() {
+
+    private lateinit var binding: FragmentShopBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentShopBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setSystemUiVisibility()
+        setListeners()
+    }
+
+    private fun setListeners() {
+        binding.toolbar.setNavigationOnClickListener { findNavController().popBackStack() }
+    }
+
+    @Suppress("DEPRECATION")
+    @SuppressLint("InlinedApi")
+    private fun setSystemUiVisibility() {
+        requireActivity().window.run {
+            decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            statusBarColor = ContextCompat.getColor(requireContext(), android.R.color.transparent)
+        }
+    }
+
+}
