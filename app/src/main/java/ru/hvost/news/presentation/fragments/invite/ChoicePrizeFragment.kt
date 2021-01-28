54ade5bd17a458e35542a2c7df81810994d69813
yunusov <yunusov@studiofact.ru>
2021-01-28 17:40:24 +0500

    Правки
    #91618#

--- app/src/main/java/ru/hvost/news/presentation/fragments/invite/ChoicePrizeFragment.kt
+++ app/src/main/java/ru/hvost/news/presentation/fragments/invite/ChoicePrizeFragment.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
